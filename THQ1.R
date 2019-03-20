0fa605bde2eb27aa326804f74e60f244d67f1388
hugo1996wu <48667195+hugo1996wu@users.noreply.github.com>
2019-03-20 11:46:01 +1100

    Update THQ1.R
    Updated version  at 1145, 20/03

--- THQ1.R
+++ THQ1.R
@@ -1,11 +1,50 @@
 # Question 1
-P <- function(C, FV, n, int){
+P <- function(C, FV, n, y){
   t <- seq(0.5, n, 0.5)
-  P <- sum(C*exp(-int*t))+FV*exp(-int[2*n]*t[n])
+  P <- sum(C*exp(-y*t))+FV*exp(-y[2*n]*t[n])
   return(P)
 }
-int <- c(0.1,0.2)
-P(10,10,1,int)
+y <- c(0.1,0.2)
+P(10,10,1,y)
 
+  
 
-
+#### Question 3 ####
+# Part a
+dataset <- read.csv(file = "singapore.economy.csv", header = T)
+# Part b
+dataset <- na.omit(dataset)
+# Part c
+dataset$time <- ts(dataset$time)
+plot(dataset$time,dataset$gdp,
+     type='l',
+     xlab='Time',ylab='GDP (%)',main='Singapore GDP growth')
+# Part d
+dataset$period <- factor(dataset$period)
+m <- aggregate(dataset$gdp, by=list(dataset$period), FUN = mean)
+s <- aggregate(dataset$gdp, by=list(dataset$period), FUN = sd)
+stat.table <- cbind(m, s[,2])
+colnames(stat.table) <- c('Period','Mean','St dev')
+stat.table
+# Part e
+pairs(dataset[,3:dim(dataset)[2]])
+# Part f
+mdl1 <- lm(gdp~exp, data = dataset)
+summary(mdl1)
+# Part g
+mdl2 <- lm(gdp~exp+epg+hpr+oil+gdpus+crd, data = dataset)
+summary(mdl2)
+# Part h
+q <- quantile(dataset$gdp,probs = 0.05)
+state <- factor(ifelse(dataset$gdp < q, "crisis", "normal"))
+dataset <- data.frame(cbind(dataset, state))
+train <- dataset[dataset$time<2008,]
+test <- dataset[dataset$time>=2008,]
+mdl3 <- glm(state~bci, data = train, family = binomial)
+summary(mdl3)
+prob <- predict(mdl3, newdata = test, type = 'response')
+contrasts(state)
+pred <- ifelse(prob<0.05, "crisis","normal")
+conf.mat <- table(pred, test$state)
+mean(pred==test$state)
+conf.mat
